9e4040dfd1eaf7bb21f214cff429323c51eefe00
Kian Zohoury <kzohoury@berkeley.edu>
2024-11-01 08:22:51 +0000

    fine-tuning for point-e

---
+++ finetune_pointE.py
@@ -0,0 +1,234 @@
+# ==============================================================================
+# Copyright (c) 2023 Tiange Luo, tiange.cs@gmail.com
+# Last modified: November 10, 2023
+#
+# This code is licensed under the MIT License.
+# ==============================================================================
+
+
+import torch
+import torch.optim as optim
+
+from tqdm.auto import tqdm
+from point_e.diffusion.configs import DIFFUSION_CONFIGS, diffusion_from_config
+from point_e.diffusion.sampler import PointCloudSampler
+from point_e.models.download import load_checkpoint
+from point_e.models.configs import MODEL_CONFIGS, model_from_config
+from point_e.util.plotting import plot_point_cloud
+
+import os
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel
+import argparse
+
+import glob
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import sys
+import pickle
+import pandas as pd
+import time
+import numpy as np
+from datetime import datetime
+from pathlib import Path
+
+
+# imports from machina-labs project
+from ..dataset import shapenetcore
+module_path = Path(__file__).parent.resolve()
+sys.path.append(str(Path(module_path, "example_material/")))
+
+def setup_ddp(gpu, args):
+    dist.init_process_group(                                   
+        backend='nccl',      # backend='gloo',#                                    
+        init_method='env://',     
+        world_size=args.world_size,                              
+        rank=gpu)
+
+    torch.manual_seed(0)
+    torch.cuda.set_device(gpu)
+
+
+# EDIT: accept ShapeNetCore dataset
+class pointE_train_dataset(Dataset):
+    def __init__(self, pts_path, split="train"):
+        # self.captions = pd.read_csv(f'{module_path}/example_material/Cap3D_automated_Objaverse.csv', header=None)
+        # self.valid_uid = list(pickle.load(open(f'{module_path}/example_material/training_set.pkl','rb')))
+        # self.final_uid = self.valid_uid
+        # self.n2idx = {}
+        # for i in range(len(self.captions)):
+        #     self.n2idx[self.captions[0][i]] = i
+        self.pts_path = pts_path
+
+        # this is the index of the 1024 points in the point cloud, randomly generated via torch.randperm(16384)[:1024]
+        # it is fixed during training
+        # you can also generate your own index via Farthest Point Sampling (FPS) algorithm
+        # self.pointcloud_1024index = torch.load('./example_material/pointE_pointcloud_1024index.pt')
+        self.dataset = shapenetcore.ShapeNetCore(
+            root=f"{module_path.parent}/Shapenetcore_benchmark",
+            split=split,
+            max_points=1024,
+            downsampling_mode="uniform",
+            input_transform=None,
+            use_rotations=False
+        )
+
+    def __len__(self):
+        # return len(self.final_uid)
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        caption = self.dataset[idx][1].lower()
+        data_tensor = self.dataset[idx][-1].permute(1, 0)
+        # create dummy color channels since the models expect (x, y, z, r, g, b) format
+        # for the last dimension
+        color_channels = torch.zeros((3, data_tensor.shape[1]))
+        data_tensor = torch.cat([data_tensor, color_channels], dim=0)
+        return {'caption': caption, 'pts': data_tensor}
+
+class pointE_val_dataset(pointE_train_dataset):
+    def __init__(self, pts_path, split="val"):
+        super(pointE_val_dataset, self).__init__(pts_path, split=split)
+
+
+def train(rank, args):
+    if args.gpus > 1:
+        setup_ddp(rank, args)
+
+    niter = args.epoch
+    batch_size = args.batch_size
+    learning_rate = args.lr
+    save_name = args.save_name
+
+    torch.manual_seed(rank+int(learning_rate*1e6)+int(datetime.now().timestamp()))
+
+    resume_flag = True if args.resume_name != 'none' else False
+    if resume_flag:
+        model_list = glob.glob('./model_ckpts/%s*.pth'%save_name)
+        idx_rank = []
+        for l in model_list:
+            idx_rank.append(int(l.split('/')[-1].split('_')[-2][5:]) * 21000 + int(l.split('/')[-1].split('_')[-1].split('.')[0]))
+        newest = np.argmax(np.array(idx_rank))
+        args.resume_name = model_list[newest].split('/')[-1].split('.')[0]
+
+    start_epoch = 0 if not resume_flag else int(args.resume_name.split('_')[-2][5:])
+    start_iter = 0 if not resume_flag else int(args.resume_name.split('_')[-1].split('.')[0])
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if resume_flag:
+        print('reload from ./model_ckpts/%s.pth'%args.resume_name)
+        checkpoint = torch.load('./model_ckpts/%s.pth'%args.resume_name, map_location=device)
+
+    base_name = 'base40M-textvec'
+    model = model_from_config(MODEL_CONFIGS[base_name], device)
+    if not resume_flag:
+        model.load_state_dict(load_checkpoint(base_name, device))
+    else:
+        model.load_state_dict(checkpoint['model_state_dict'])
+    model.train()
+    if args.gpus > 1:
+        model = DistributedDataParallel(
+                model, device_ids=[rank], find_unused_parameters=False
+        )
+    
+    diffusion = diffusion_from_config(DIFFUSION_CONFIGS[base_name])
+    my_dataset_train = pointE_train_dataset(args.dataset_path)
+    data_loader = DataLoader(my_dataset_train, batch_size=batch_size, shuffle=True, drop_last=True)
+    my_dataset_val = pointE_val_dataset(args.dataset_path)
+    data_loader_val = DataLoader(my_dataset_val, batch_size=batch_size, num_workers=8, prefetch_factor=4, drop_last=True)
+    optimizer= optim.AdamW(model.parameters(), lr=learning_rate)
+    total_iter_per_epoch = int(len(my_dataset_train)/batch_size)
+    lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, niter*total_iter_per_epoch)
+    if resume_flag:
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        lr_scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+
+    best_val_loss = float("inf")
+    for epoch in range(start_epoch, niter):
+        s = time.time()
+        for i, data in enumerate(data_loader):
+            if i + start_iter == total_iter_per_epoch:
+                start_iter = 0
+                break
+            s2 = time.time()
+            prompt = data['caption']
+            model_kwargs=dict(texts=prompt)
+            t = torch.randint(0, DIFFUSION_CONFIGS[base_name]['timesteps'], size=(batch_size,), device=device) 
+            x_start = data['pts'].cuda()
+
+            optimizer.zero_grad()
+            loss = diffusion.training_losses(model, x_start, t, model_kwargs=model_kwargs)
+            final_loss = torch.mean(loss['loss'])
+
+            skip_step = torch.isnan(final_loss.detach()) or not torch.isfinite(final_loss.detach())
+            skip_step_tensor = torch.tensor(skip_step, dtype=torch.int).to(device)
+            if args.gpus > 1:
+                dist.all_reduce(skip_step_tensor, op=dist.ReduceOp.SUM)
+            skip_step = skip_step_tensor.item() > 0
+            if skip_step:
+                del final_loss
+                torch.cuda.empty_cache()
+            else:
+                final_loss.backward()
+                optimizer.step()
+                lr_scheduler.step()
+                if args.gpus == 1 or (args.gpus >1 and dist.get_rank() == 0):
+                    print('rank: ',rank,time.time()-s2,' epoch: ', epoch, i, final_loss.item())
+                if i%100 == 0:
+                    with torch.no_grad():
+                        val_loss = []
+                        for j, dataval in enumerate(data_loader_val):
+                            prompt = data['caption']
+                            model_kwargs=dict(texts=prompt)
+                            t = torch.randint(0, DIFFUSION_CONFIGS[base_name]['timesteps'], size=(batch_size,), device=device) 
+                            x_start = data['pts'].cuda()
+                            loss = diffusion.training_losses(model, x_start, t, model_kwargs=model_kwargs)
+                            final_loss = torch.mean(loss['loss'])
+                            print('validation %d/%d: '%(j, len(data_loader_val)), final_loss.item())
+                            val_loss.append(final_loss.item())
+                        val_mean_loss = torch.mean(torch.Tensor(val_loss)).item()
+                        print('rank: ',rank, i, val_mean_loss)
+                        
+                # EDIT: save best model only
+                if val_mean_loss < best_val_loss:
+                    best_val_loss = val_mean_loss
+                    print("Saving best model...")
+                    checkpoint_dir = Path(f"{module_path.parent}/checkpoints")
+                    if not checkpoint_dir.exists():
+                        checkpoint_dir.mkdir()
+                        
+                    # add fix for non distributed training
+                    if hasattr(model, "module"):
+                        model_state_dict = model.module.state_dict()
+                    else:
+                        model_state_dict = model.state_dict()
+                    torch.save({'model_state_dict': model_state_dict, 
+                                'optimizer_state_dict': optimizer.state_dict(),
+                                'scheduler_state_dict': lr_scheduler.state_dict(),
+                                }, f"{str(checkpoint_dir)}/{save_name}.pth")
+
+# EDIT: change default training args
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    model_group = parser.add_argument_group('Model settings')
+    model_group.add_argument('--port', type = str, default = '12356', help = 'port for parallel')
+    model_group.add_argument('--gpus', type = int, default = 1, help = 'how many gpu use')
+    model_group.add_argument('--resume_name', type = str, default = 'none', help = 'any name different from "none" will resume the training')
+    model_group.add_argument('--save_name', type = str, default = 'defect_diffusion', help = 'name for the save file')
+    model_group.add_argument('--lr', type = float, default = 1e-5, help = 'learning rate')
+    model_group.add_argument('--batch_size', type = int, default = 64, help = 'batch size')
+    model_group.add_argument('--epoch', type = int, default = 1, help = 'total epoch')
+    # modified
+    model_group.add_argument('--dataset-path', type = str, default = '../Shapenetcore_benchmark', help = 'the directory where the point clouds are stored')
+
+    args = parser.parse_args()
+    if args.gpus == 1:
+        train(args.gpus, args)
+    else:
+        os.environ['MASTER_ADDR'] = 'localhost'
+        os.environ['MASTER_PORT'] = args.port
+        args.world_size = args.gpus
+        mp.spawn(train, nprocs=args.gpus, args=(args,))
+
+
---
+++ text2ply_pointE.py
@@ -0,0 +1,139 @@
+# ==============================================================================
+# Copyright (c) 2023 Tiange Luo, tiange.cs@gmail.com
+# Last modified: September 20, 2023
+#
+# This code is licensed under the MIT License.
+# ==============================================================================
+import torch
+from tqdm.auto import tqdm
+
+from point_e.diffusion.configs import DIFFUSION_CONFIGS, diffusion_from_config
+from point_e.diffusion.sampler import PointCloudSampler
+from point_e.models.download import load_checkpoint, fetch_file_cached
+from point_e.models.configs import MODEL_CONFIGS, model_from_config
+from point_e.util.plotting import plot_point_cloud
+
+import os
+import sys
+from PIL import Image
+import torch
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+from pathlib import Path
+
+from point_e.models.download import load_checkpoint
+from point_e.models.configs import MODEL_CONFIGS, model_from_config
+from point_e.util.pc_to_mesh import marching_cubes_mesh
+from point_e.util.plotting import plot_point_cloud
+from point_e.util.point_cloud import PointCloud
+from point_e.util.mesh import TriMesh
+import numpy as np
+import trimesh
+import pickle
+
+import argparse
+import random
+import time
+
+# imports from machina-labs project
+from ..dataset import shapenetcore
+module_path = Path(__file__).parent.resolve()
+sys.path.append(str(Path(module_path, "checkpoints/")))
+Path(module_path.parent, "synthetic_data").mkdir(exist_ok=True)
+
+# EDIT: change defaults
+parser = argparse.ArgumentParser()
+parser.add_argument('--ckpt', default=Path(
+    module_path.parent, "checkpoints", "defect_diffusion.pth"
+), type=str, help="path to finetuned model")
+
+parser.add_argument('--save_name', default=Path(module_path.parent, "synthetic_data"), type=str, help="result files save to here")
+parser.add_argument('--num_generate', default=1, type=str, help="number of point clouds to generate")
+opt = parser.parse_args()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+print('creating base model...')
+base_name = 'base40M-textvec'
+base_model = model_from_config(MODEL_CONFIGS[base_name], device)
+base_model.eval()
+base_diffusion = diffusion_from_config(DIFFUSION_CONFIGS[base_name])
+
+print('creating upsample model...')
+upsampler_model = model_from_config(MODEL_CONFIGS['upsample'], device)
+upsampler_model.eval()
+upsampler_diffusion = diffusion_from_config(DIFFUSION_CONFIGS['upsample'])
+
+print('loading finetuned checkpoint: ', opt.ckpt)
+base_model.load_state_dict(torch.load(os.path.join('./model_ckpts', opt.ckpt), map_location=device)['model_state_dict'])
+
+### results (.ply) saved here
+num_generate = opt.num_generate
+# save_dir = os.path.join('./pointE_inference', opt.save_name)
+# os.makedirs(save_dir, exist_ok=True)
+
+print('downloading upsampler checkpoint...')
+upsampler_model.load_state_dict(load_checkpoint('upsample', device))
+
+sampler = PointCloudSampler(
+    device=device,
+    models=[base_model, upsampler_model],
+    diffusions=[base_diffusion, upsampler_diffusion],
+    num_points=[1024, 4096 - 1024],
+    aux_channels=['R', 'G', 'B'],
+    guidance_scale=[3.0, 0.0],
+    model_kwargs_key_filter=('texts', ''), # Do not condition the upsampler at all
+)
+
+print('creating SDF model...')
+name = 'sdf'
+model = model_from_config(MODEL_CONFIGS[name], device)
+model.eval()
+print('loading SDF model...')
+model.load_state_dict(load_checkpoint(name, device))
+
+batch_size = 1
+
+import pickle
+import pandas as pd
+
+# EDIT: test on ShapeNetCore dataset
+test_dataset = shapenetcore.ShapeNetCore(
+    root=f"{module_path.parent}/Shapenetcore_benchmark",
+    split="test",
+    max_points=1024,
+    downsampling_mode="uniform",
+    input_transform=None,
+    use_rotations=False
+)
+
+test_uids = list(range(len(test_dataset)))
+### add the below random command to parallel test
+random.shuffle(test_uids)
+test_uids = test_uids[:num_generate]
+
+print('start mesh generation, generated mesh saved as .ply')
+for i in range(len(test_uids)):
+    s = time.time()
+    ### skip if output mesh exists
+    if os.path.exists(os.path.join(save_dir,'%s.ply'%(test_uids[i]))):
+       continue
+   # only need a test prompt since we're generating point clouds
+    prompt = test_dataset[i][1]
+    samples = None
+    for x in tqdm(sampler.sample_batch_progressive(batch_size=batch_size, model_kwargs=dict(texts=[prompt,]*batch_size))):
+        samples = x
+
+    pc = sampler.output_to_point_clouds(samples)[0]
+
+    mesh = marching_cubes_mesh(
+        pc=pc,
+        model=model,
+        batch_size=4096,
+        grid_size=128, 
+        progress=True,
+    )
+
+    with open(os.path.join(save_dir, '%s.ply'%(test_uids[i])), 'wb') as f:
+        mesh.write_ply(f)
+    print('mesh generation progress: %d/%d'%(i,len(test_uids)), 'time cost:', time.time()-s)
